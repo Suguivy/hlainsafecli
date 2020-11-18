8adfd498ab1bc409745aab5841f113408dadb4e6
Ivy <adrisolgo7373@gmail.com>
2020-11-18 21:57:37 +0100

    Added cmd options, custom url and multiple files

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,66 @@
 module Main where
 
+import Control.Monad
+import System.IO
+import System.Console.GetOpt
 import System.Environment
 import System.Directory
+import System.Exit
 import Lib
+
+data Options = Options { showHelp       :: Bool
+                       , printLainQuote :: Bool
+                       }
+
+defaultOptions :: Options
+defaultOptions = Options { showHelp       = False
+                         , printLainQuote = True
+                         }
+
+options :: [OptDescr (Options -> IO Options)]
+options = [ Option "h" ["help"]         (NoArg (\_ -> do
+                                                   hPutStr stderr $ usageInfo "hlainsafe" options
+                                                   exitSuccess )) "show help"
+          , Option "l" ["no-lain"]      (NoArg (\opt -> return opt { printLainQuote = False })) "do not print a lain quote"
+          ]
+
+parseOptions :: [String] -> IO (Options, [String])
+parseOptions args = do
+  let (actions, noOpt, errors) = getOpt RequireOrder options args
+  case errors of
+    [] -> do opts <- foldl (>>=) (return defaultOptions) actions
+             return (opts, noOpt)
+    _  -> do hPutStr stderr $ head errors
+             exitFailure
+
+incompleteArgs :: [String] -> Bool
+incompleteArgs a
+  | length a < 2 = True
+  | otherwise    = False
 
 main :: IO ()
 main = do
-  args <- getArgs
-  if null args then
-    putStrLn "No file provided"
-  else do
-    let file = head args
-    fileExists <- doesFileExist file
-    if fileExists then
-      uploadRequest (head args) >>= putStrLn
-    else putStrLn "Incorrect filepath"
+  (opts, args) <- getArgs >>= parseOptions
+  progName <- getProgName
+
+  when (incompleteArgs args) $ do
+    putStrLn "error: missing arguments"
+    putStrLn $ "usage: " ++ progName ++ " [OPTION] URL FILE [FILE [FILE ...]]"
+    exitSuccess
+
+  let (url:files) = args
+
+  when (showHelp opts) $ do
+    hPutStr stderr $ usageInfo progName options
+    exitSuccess
+
+  exist <- and <$> sequence (doesFileExist <$> files)
+  unless exist $ do
+    putStrLn "error: incorrect filepath"
+    exitFailure
+
+  urls <- mapM (uploadRequest url) files
+  mapM_ (\(f,u) -> putStrLn (f ++ ": " ++ u)) $ zip files urls
+
+  when (printLainQuote opts) $
+    putStrLn "\n\"No matter where you go, everyone's connected.\""
--- src/Lib.hs
+++ src/Lib.hs
@@ -6,11 +6,13 @@ import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
 import           Network.HTTP.Client.MultipartFormData
 
-uploadRequest :: FilePath -> IO String
-uploadRequest file = do
+type Url = String
+
+uploadRequest :: Url -> FilePath -> IO String
+uploadRequest url file = do
   req <- (formDataBody [partFile "file" file] $
-         (parseRequest_ "https://lainsafe.delegao.moe/upload.cgi") {method="POST"
-                                                                   , requestHeaders = [("Content-Type", "multipart/form-data")]})
+         (parseRequest_ url) {method="POST"
+                             , requestHeaders = [("Content-Type", "multipart/form-data")]})
   man <- newManager tlsManagerSettings
   response <- httpLbs req man
   return . L8.unpack $ responseBody response
